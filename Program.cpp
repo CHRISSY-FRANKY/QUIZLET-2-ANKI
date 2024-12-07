0ff4776726b892c2b026107c6280839278924b3b
Christopher Francisco <cjfg00gl3@gmail.com>
2024-12-07 15:12:01 -0600

    CREATE Program.cpp to serve as the boiler plate

---
+++ Program.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+char** getUnFormattedText()
+{
+    return NULL;
+}
+
+char** getFormattedText()
+{
+    return NULL;
+}
+
+char** exportFormattedText()
+{
+    return NULL;
+} 
+
+int main(void)
+{
+    std::cout << "Welcome To Quizlet Cloner!" << std::endl;
+    return 0;
+}
