432ecdbbb44388c5a7f728fd6f8fbaddcc91e509
Christopher Francisco <cjfg00gl3@gmail.com>
2024-12-07 15:23:59 -0600

    CREATE Program.c DELETE Program.cpp

---
+++ Program.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+char** getUnFormattedText()
+{
+    return NULL;
+}
+
+char** getFormattedText(char** unFormattedText)
+{
+    return NULL;
+}
+
+char** generateFormattedTextDirectoryFile(char** formattedText)
+{
+    return NULL;
+}
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
