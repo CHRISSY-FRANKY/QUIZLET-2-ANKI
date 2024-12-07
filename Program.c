070615b87ead307d230bc519042c1b797de63f00
Christopher Francisco <cjfg00gl3@gmail.com>
2024-12-07 17:24:19 -0600

    UPDATE Program.c by updating getUnFormattedContent function to open the file, allocate memory for the file contents, and read the file contents

--- Program.c
+++ Program.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char* getUnFormattedText(char* filename)
+char* getUnFormattedContent(char* filename)
 {
-    
-    return NULL;
+    FILE* file = fopen(filename, "rb"); // open the file
+    if (file == NULL) // Opening file failed
+    {
+        fprintf(stderr, "Opening file %s FAILED!", filename);
+        return NULL;
+    }
+    fseek(file, 0, SEEK_END); // Set file pointer to the end
+    unsigned int size = ftell(file); // Determine file size
+    fseek(file, 0, SEEK_SET); // Reset file pointer to the start
+    char* unFormattedContent = malloc(sizeof(char) * (size + 1)); // Allocate for file content and null terminator
+    if (unFormattedContent == NULL) // Memory allocation failed
+    {
+        fprintf(stderr, "Allocating memory FAILED!");
+        return NULL;
+    }
+    size_t read_size = fread(unFormattedContent, 1, size, file); // Read file contents
+    if (read_size != size) // Didn't read entire file
+    {
+        fprintf(stderr, "Reading file FAILED!");
+        free(unFormattedContent); // no leaks
+        fclose(file);
+        return NULL;
+    }
+    unFormattedContent[size] = '\0'; // add the null terminator
+    fclose(file);
+    return unFormattedContent;
 }
 
 char* getFormattedText(char** unFormattedText)
@@ -25,7 +49,7 @@ int main(int nvar, char** vars)
         fprintf(stderr, "Must enter file directory after filename %s", vars[0]);
         return 1;
     } else {
-
+        char* unFormattedContent = getUnFormattedContent(vars[1]);
     }
     return 0;
 }
