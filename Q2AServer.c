e421f35d2563066a178395a10e35007927d1af49
Christopher Francisco <cjfg00gl3@gmail.com>
2024-12-15 20:48:40 -0600

    UPDATE Q2AServer.c to ensure the indexResponse function ensures the first call is the headers call

--- Q2AServer.c
+++ Q2AServer.c
@@ -4,8 +4,15 @@
 
 #define PORT 8080
 
-static int indexResponse(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls)
+static int indexResponse(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **connection_closure)
 {
+    static int initialConnectionCall;
+    if (initialConnectionCall != *connection_closure) // Check if this is the initial connection call / headers only
+    {
+        *connection_closure = initialConnectionCall;
+        return MHD_YES;
+    }
+
     return 0;
 }
 
