78ef10fc85187e139dfa7bead5da91c410a6d689
Christopher Francisco <cjfg00gl3@gmail.com>
2024-12-15 20:18:36 -0600

    UPDATE Q2AServer.c by creating the skeleton for the indexResponse function

--- Q2AServer.c
+++ Q2AServer.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <microhttpd.h>
 
+#define PORT 8080
+
+static int indexResponse(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls)
+{
+    return 0;
+}
+
 int main(void)
 {
     printf("Hello, World!\n");
